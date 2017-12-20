136ddd93c00f32089b4d47fce1ee0efe20835dc5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-19 23:50:33 -0700

    Make a basic User model

---
+++ db/migrate/20171220063527_fix_password_digest_typo.rb
@@ -0,0 +1,5 @@
+class FixPasswordDigestTypo < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :users, :password_disgest, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220044322) do
+ActiveRecord::Schema.define(version: 20171220063527) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_disgest"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
