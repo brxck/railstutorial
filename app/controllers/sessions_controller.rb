eb1f34420c6c251088e6e786547c9e6c752e5ce3
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-30 20:04:44 -0700

    Implement account activation.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user # uses our method from sessions_helper
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user # uses our method from sessions_helper
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message = "Account not activated."
+        message += "Check your email to activate your account."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # flash.now makes sure that the flash is displayed after rendering, 
       # not after the next http request.
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users, except: [:new, :create]
+  resources :users
+  resources :account_activations, only: [:edit]
 end
