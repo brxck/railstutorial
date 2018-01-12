4faca4271741026c809e5840a38506f2de3377c7
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-11 20:14:06 -0700

    Add user microposts

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,11 +10,11 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email will be sent with password reset instructions."
+      flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email not found."
-      render :new
+      flash.now[:danger] = "Email address not found"
+      render 'new'
     end
   end
 
